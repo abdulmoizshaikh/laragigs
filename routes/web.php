14030c3fb984fd4436ce25029192766847357c2f
muhammadmoizshaikh <muhammad.moiz@digitaltolk.com>
2023-11-03 16:50:36 +0500

    laravel blade and migration and seeding work

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// the route for this will be this http://laragigs.test/hello and it prints Hello World on screen
+Route::get('/hello', function () {
+    return response("<h1>Hello World</h1>")
+        ->header('Content-Type', 'text/plain')
+        ->header('foo', 'bar');
+});
+
+// {id} whilecard id
+// ->where("id", '[0~9]+'); constraints
+// dd die and dump helper method for debugging this will stop everything and show me that whatever pass in
+// ddd Dump, Die, Debug -> this will show more details information
+Route::get('/posts/{id}', function ($id) {
+    // dd($id);
+    // ddd($id);
+    return response("Post " . $id);
+})->where("id", '[0-9]+');
+
+
+Route::get('/search', function (Request $request) {
+    return $request->name . ' ' . $request->city;
 });
