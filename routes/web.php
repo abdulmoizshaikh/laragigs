785a11a6cf7eed324d3b3aaa17d1b0e8245d6e20
muhammadmoizshaikh <muhammad.moiz@digitaltolk.com>
2023-11-06 16:14:08 +0500

    Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ListingController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -42,15 +43,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
-
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+// Single Listing
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
