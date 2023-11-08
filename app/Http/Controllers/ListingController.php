56dc630cd5e941df7195f3a94cc12df9df6b894a
muhammadmoizshaikh <muhammad.moiz@digitaltolk.com>
2023-11-08 17:14:30 +0500

    Add Ownership to Listings

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -57,6 +57,9 @@ class ListingController extends Controller
             );
         }
 
+        $formFields['user_id'] = auth()->id();
+
+
         Listing::create($formFields);
 
         return redirect('/')->with('message', 'Listing created successfully!');
